d167fc5f899a847043c1de2fd65be642eac7fdc7
xsy <480020480@qq.com>
2020-12-24 22:39:51 +0800

    a

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -33,17 +33,17 @@ export default {
         MP("QV1kPbVFSpewha0E9TFir4RckLARtKI9").then(BMap => {
           // 百度地图API功能
       // 创建地图实例  =
-      var map = new window.BMap.Map("container");
+      var map = new BMap.Map("container");
       // 创建点坐标  
-      var point = new window.BMap.Point(116.404, 39.915);
+      var point = new BMap.Point(116.404, 39.915);
       // 初始化地图，设置中心点坐标和地图级别  
       map.centerAndZoom(point, 5);
       // 允许鼠标滚轮缩放
       map.enableScrollWheelZoom(true);
 
       map.addEventListener('click', function (e) {
-        let myGeo = new window.BMap.Geocoder();
-        let currentPoint = new window.BMap.Point(e.point.lng, e.point.lat);
+        let myGeo = new BMap.Geocoder();
+        let currentPoint = new BMap.Point(e.point.lng, e.point.lat);
         myGeo.getLocation(currentPoint, function(info){
             alert(info.addressComponents.province)
         })
