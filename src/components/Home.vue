3f5a92d7d40571165556532bd10461ee9af4c0e6
xsy <480020480@qq.com>
2020-12-24 22:10:36 +0800

    add map

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,39 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template> 
+	<div id="container"></div> 
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+<script> 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'Home', 
+  methods: { 
+    drawMap: function () { 
+      // 创建地图实例  =
+      var map = new BMap.Map("container");
+      // 创建点坐标  
+      var point = new BMap.Point(116.404, 39.915);
+      // 初始化地图，设置中心点坐标和地图级别  
+      map.centerAndZoom(point, 5);
+      // 允许鼠标滚轮缩放
+      map.enableScrollWheelZoom(true);
+
+      map.addEventListener('click', function (e) {
+        let myGeo = new BMap.Geocoder();
+        let currentPoint = new BMap.Point(e.point.lng, e.point.lat);
+        myGeo.getLocation(currentPoint, function(info){
+            alert(info.addressComponents.province)
+        })
+      });
+    } 
+  }, 
+  mounted(){ 
+    //调用上面的函数
+    this.drawMap() 
+  } 
+} 
+</script> 
+
+<style>
+#container { 
+  height: 99vh;
+}   
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
+import About from './components/About.vue'
 
 Vue.use(Router)
 
@@ -17,7 +18,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './components/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './components/About.vue')
+      component: About
     }
   ]
 })
